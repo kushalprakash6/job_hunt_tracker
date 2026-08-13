bc80078b4bf97e7ab337d8b6727dda6040630012
kushalprakash6 <kushal.prakash@yahoo.in>
2026-08-13 21:10:40 +0200

    Added structs for required fields and added dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Status {
+    NotApplied,
+    Applied,
+    FirstCallTelephone,
+    FirstCallVideo,
+    FurtherRounds,
+    Selected,
+    Rejected,
+    ReminderSent,
+}
+
+#[derive(Clone)]
+struct Application {
+    id: i64,
+    company: String,
+    location: String,
+    title: String,
+    link: String,
+    application_date: String,
+    status: Status,
+    contact_name: String,
+    contact_phone: String,
+    contact_email: String,
+    notes: String,
+    last_contact: Option<String>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
